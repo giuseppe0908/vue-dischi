26dd2615c609488a2acd7def85e0eba50341eb57
giuseppe <placidagiuseppe@gmail.com>
2021-04-12 11:20:25 +0200

    bonus completati

--- assets/main.js
+++ assets/main.js
@@ -3,7 +3,9 @@
     el: '#root',
     data: {
       disks: [],
+      genereoption: [],
       genere:"",
+      flag: false,
 
     },
 
@@ -12,12 +14,25 @@
           .then((response) =>{
             console.log(response.data.response);
             this.disks = response.data.response;
-        //Ordinamento crescente dell'anno di uscita
-            this.disks.sort((a,b) => {
-                    return a.year - b.year;
-            })
+
+            this.disks.forEach((item, i) => {
+              if (!this.genereoption.includes(item.genre)) {
+                  this.genereoption.push(item.genre);
+              }
+            });
 
           });
+    },
+    methods: {
+      ordinamento: function () {
+        this.flag = !this.flag;
+        if (this.flag) {
+          //Ordinamento crescente dell'anno di uscita
+              this.disks.sort((a,b) =>  a.year - b.year);
+        }else {
+          this.disks.sort((a,b) =>  b.year - a.year);                                 
+        }
+      }
     }
 
   });
